31af2e91945177db40090f357d198c9f64e43ebd
ryan_annalect <ryan.mchale@annalect.com>
2019-04-13 23:33:04 -0400

    save dialog and save to external directory

--- app/src/main/java/com/notepad/garian/notepad/MainActivity.java
+++ app/src/main/java/com/notepad/garian/notepad/MainActivity.java
@@ -1,7 +1,12 @@
 package com.notepad.garian.notepad;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
 import android.content.Intent;
 import android.os.Environment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +15,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -17,9 +23,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SaveNameDialogFragment.SaveNameDialogListener{
 
     public static final String EXTRA_MESSAGE = "com.notepad.garian.notepad.MESSAGE";
+    public static final int WRITE_EXTERNAL_STORAGE_CODE=7;
+    private DialogFragment saveDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,27 +49,13 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("new page");
                 return true;
             case R.id.save_page:
-                try {
-                    Log.e("test tag","save page");
-                    if (isExternalStorageWritable()) {
-                        //File filesDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+"/notepad");
-                        File filesDir = new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)+"/notepad");
-                        //File filesDir = new File(getFilesDir()+"/notepad");
-                        filesDir.mkdirs();
-                        File newFile = new File(filesDir, "artnotes.txt");
-                        Log.e("test tag", "newFile is " + newFile);
-                        if(!newFile.exists())
-                            newFile.createNewFile();
-                        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(newFile)));
-                        EditText editText = (EditText) findViewById(R.id.editText);
-                        String contents = editText.getText().toString();
-                        bw.write(contents);
-                        bw.close();
-                    } else {
-                        Log.e("test tag","popup Storage not available");
-                    }
-                    return true;
-                }catch(IOException ioe){ioe.printStackTrace(); return false;}
+                TextView titleField = (TextView) findViewById(R.id.titleText);
+                CharSequence currentName = titleField.getText();
+                DialogFragment fragment = new SaveNameDialogFragment();
+                fragment.show(getSupportFragmentManager(),"File Name");
+                Bundle bundl = new Bundle();
+                bundl.putCharSequence("currentName",currentName);
+                fragment.setArguments(bundl);
             case R.id.load_page:
                 System.out.println("load page");
                 return true;
@@ -81,10 +75,79 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
-    public void savePad(View view){
-        
+    public boolean savePad(String fileName){
+        try {
+            Log.e("test tag","save page");
+            if (isExternalStorageWritable()) {
+
+                //File filesDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+"/notepad");
+                /*File filesDirName = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),fileName);
+                Log.e("save file","filesDirName is "+filesDirName);
+                filesDirName.createNewFile();*/
+                //File filesDir = new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)+"/notepad");
+                File filesDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)+"/notepad");
+                //File filesDir = new File(getFilesDir()+"/notepad");
+                filesDir.mkdirs();
+                File newFile = new File(filesDir, fileName);
+                Log.e("test tag", "newFile is " + newFile);
+                newFile.createNewFile();
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(newFile)));
+                EditText editText = (EditText) findViewById(R.id.editText);
+                String contents = editText.getText().toString();
+                bw.write(contents);
+                bw.close();
+            } else {
+                Log.e("test tag","popup Storage not available");
+            }
+            return true;
+        }catch(IOException ioe){ioe.printStackTrace(); return false;}
     }
 
+    public void onSavePositiveClick(DialogFragment dialogFragment){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            setSaveDialog(dialogFragment);
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},WRITE_EXTERNAL_STORAGE_CODE);
+        }else {
+            EditText filenameField = (EditText) dialogFragment.getDialog().findViewById(R.id.filenameField);
+            String filename = filenameField.getText().toString();
+            if(!filename.endsWith(".txt"))
+                filename += ".txt";
+            if (savePad(filename)) {
+                Log.e("log", filename + " saved");
+                TextView titleField = (TextView) findViewById(R.id.titleText);
+                titleField.setText(filename);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String[] permissions, int[] grantResults){
+        switch(requestCode){
+            case WRITE_EXTERNAL_STORAGE_CODE:
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    DialogFragment dialogFragment = getSaveDialog();
+                    EditText filenameField = (EditText) dialogFragment.getDialog().findViewById(R.id.filenameField);
+                    String filename = filenameField.getText().toString() + ".txt";
+                    if (savePad(filename)) {
+                        Log.e("log", filename + " saved");
+                        TextView titleField = (TextView) findViewById(R.id.titleText);
+                        titleField.setText(filename);
+                    }
+                }
+                return;
+        }
+    }
+
+    public void onSaveNegativeClick(DialogFragment dialog){
+
+    }
+
+    public void setSaveDialog(DialogFragment fragment){
+        saveDialog = fragment;
+    }
+    public DialogFragment getSaveDialog(){
+        return saveDialog;
+    }
     public void sendMessage(View view){
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editTextOld);
---
+++ app/src/main/java/com/notepad/garian/notepad/SaveNameDialogFragment.java
@@ -0,0 +1,61 @@
+package com.notepad.garian.notepad;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.support.v4.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+
+public class SaveNameDialogFragment extends DialogFragment {
+
+    public interface SaveNameDialogListener{
+        public void onSavePositiveClick(DialogFragment dialog);
+        public void onSaveNegativeClick(DialogFragment dialog);
+    }
+
+    SaveNameDialogListener listener;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.dialog_filename)
+                .setView(inflater.inflate(R.layout.dialog_filename, null))
+                .setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.e("setting positive button",SaveNameDialogFragment.this.toString());
+                        listener.onSavePositiveClick(SaveNameDialogFragment.this);
+                    }
+                })
+                .setNegativeButton(R.string.back, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        listener.onSaveNegativeClick(SaveNameDialogFragment.this);
+                    }
+                });
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        try{
+            listener = (SaveNameDialogListener)context;
+        }catch(ClassCastException cce){
+            throw new ClassCastException("activity must implement SaveNameDialogListener");
+        }
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        EditText titleField = (EditText) this.getDialog().findViewById(R.id.filenameField);
+        titleField.setText((CharSequence) getArguments().get("currentName"));
+    }
+}
